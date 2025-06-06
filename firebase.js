b3c7212a935bc30d2c9c45f4ae6aac0b7bb7775d
Zizuhrez <109472715+Zizuhrez@users.noreply.github.com>
2025-06-06 03:47:25 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,10 +2,13 @@ const firebaseConfig = {
   apiKey: "AIzaSyCBZieIBuGS_ZTIQgedZCQgV5zg8KxexUQ",
   authDomain: "trimmy-9befa.firebaseapp.com",
   projectId: "trimmy-9befa",
-  storageBucket: "trimmy-9befa.firebasestorage.app",
+  storageBucket: "trimmy-9befa.appspot.com", // ✅ fixed here
   messagingSenderId: "1004261096048",
   appId: "1:1004261096048:web:b1919461d044a91f6892b5",
   measurementId: "G-TCTDHB8SWL"
 };
+
 firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+
+
+window.db = firebase.firestore();
